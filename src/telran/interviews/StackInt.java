6e2aa7a5028b20ffc61ca4fd64a3328f8423d233
Yuragranovsky <yuragranovsky@gmail.com>
2023-07-12 21:19:25 +0300

    CW #29 Standard List / Stack / Queue

---
+++ src/telran/interviews/StackInt.java
@@ -0,0 +1,45 @@
+package telran.interviews;
+
+/**
+ * 
+ * All methods have to have the Complexity O[1]
+ *
+ */
+public class StackInt {
+//TODO fields
+	/**
+	 * 
+	 * @param num
+	 * adds num in the stack
+	 */
+	public void push(int num) {
+		//TODO
+	}
+	/**
+	 * 
+	 * @return
+	 * takes out and returns the top stack number
+	 * in the case the stack is empty the exception of the class NoSuchElement should be thrown
+	 */
+	public int pop() {
+		//TODO
+		return 0;
+	}
+	/**
+	 * 
+	 * @return true if the stack is empty
+	 */
+	public boolean isEmpty() {
+		//TODO
+		return false;
+	}
+	/**
+	 * 
+	 * @return maximal value existing in the stack
+	 * throws NoSuchElementException in the case the stack is empty
+	 */
+	public int max() {
+		//TODO
+		return 0;
+	}
+}
